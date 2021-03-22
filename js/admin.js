a3b5a45300462a3a169370124df1fbb030813255
Florencia norry <maflorencia.norry@gmail.com>
2021-03-22 17:20:52 -0300

    se crea clase Juegos y se agrega evento para agregar un Juego

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,26 @@
-import {Juegos} from './ClasesAdmin.js';
+import { Juegos } from "./ClasesAdmin.js";
 
-let nuevoJuego = new Juegos(1, 'Assassins Creed', 'Un jugador', 'El mejor juego');
-console.log(nuevoJuego);
+let arregloJuegos = [];
 
-// let codigo = document.getElementById('codigo');
-// codigo.addEventListener('click',CampoRequerido);
+// let boton = document.getElementById('btnAgregar');
+// boton.addEventListener('click',CampoRequerido);
+
+window.agregarJuego = function (event) {
+  event.preventDefault();
+  console.log("agregar juego");
+  let codigo = document.getElementById("codigo").value;
+  let nombre = document.getElementById("nombre").value;
+  let categoria = document.getElementById("categoria").value;
+  let descripcion = document.getElementById("descripcion").value;
+  let publicado = document.getElementById("publicado").value;
+
+  let nuevoJuego = new Juegos(
+    codigo,
+    nombre,
+    categoria,
+    descripcion,
+    publicado
+  );
+  arregloJuegos.push(nuevoJuego);
+  console.log(arregloJuegos);
+};
