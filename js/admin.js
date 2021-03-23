13c9442d6a0b6e320741dd0cbed521b99e649914
Florencia norry <maflorencia.norry@gmail.com>
2021-03-23 01:41:15 -0300

    se crea funcion para cargar los juegos en la tabla

--- js/admin.js
+++ js/admin.js
@@ -42,6 +42,8 @@ window.agregarJuego = function (event) {
     'El juego fue guardado!',
     'success'
   );
+  datosLocalStorage();
+ 
   //cerrar la ventana modal
   modalJuego.hide();
 };
@@ -56,13 +58,54 @@ function limpiarForm() {
 }
 
 //funcion leer los datos de local y que no se borren
-function datosLocalStorage(){
-  if(localStorage.length > 0){
+function datosLocalStorage() {
+  if (localStorage.length > 0) {
     // se trae con variable provisoria los datos de localStorrage
     //parse transforma Json a java script 
     let _arregloJuegos = JSON.parse(localStorage.getItem('ListaDeJuegos'));
-    console.log(_arregloJuegos);  
+    console.log(_arregloJuegos);
+    //si el arreglo esta vacio, le cargo los datos de Local
+    if (arregloJuegos.length === 0) {
+      arregloJuegos = _arregloJuegos;
+    }
+    cargarTabla (_arregloJuegos);
   }
+}
 
-  
+function cargarTabla(_arregloJuegos) {
+  let tablaJuegos = document.getElementById('tablaJuegos');
+  let filaJuego = '';
+  //limpia los datos del tbody
+  tablaJuegos.innerHTML = '';
+
+  for (let i in _arregloJuegos) {
+    filaJuego = ` <tr>
+    <th scope="row">${_arregloJuegos[i].codigo}</th>
+    <td>${_arregloJuegos[i].nombre}</td>
+    <td>${_arregloJuegos[i].categoria}</td>
+    <td>${_arregloJuegos[i].descripcion}</td>
+    <td>${_arregloJuegos[i].imagen}</td>
+    <td>
+      <div class="form-check">
+        <input
+          type="checkbox"
+          id="publicado"
+          class="form-check-input"
+        />
+      </div>
+    </td>
+    <td>
+      <button class="iconos">
+        <i class="publicado far fa-star"></i>
+      </button>
+      <button class="iconos">
+        <i class="editar far fa-edit"></i>
+      </button>
+      <button class="iconos">
+        <i class="eliminar fas fa-trash-alt"></i>
+      </button>
+    </td>
+  </tr>`;
+    tablaJuegos.innerHTML += filaJuego;
+  }
 }
