06342b55dc1aabc42a2d35f18d08d6e7cdbcce0c
Slaya M. Ostapczuk <slaya.ostapczuk@gmail.com>
2021-03-26 10:13:14 -0300

    pude hacer la logica para rellenar los nombres de cada juego

--- js/detalle.js
+++ js/detalle.js
@@ -4,12 +4,14 @@ leerdatoslocal();
 function leerdatoslocal() {
     if (localStorage.length > 0) {
         juegostienda = JSON.parse(localStorage.getItem('ListaDeJuegos'));
-        let nombre = document.getElementById(tituloJuego);
+        console.log('local', juegostienda)
+        let nombre = document.getElementById('tituloJuego');
         nombre.innerHTML = '';
         for (let i in juegostienda) {
+            console.log('iii', i)
             let titulo =
                 `<section class="text-center" id="tituloJuego">
-            <h2 class="jumbotron bg-dark display-3 pt-5 text-light">${juegostienda.nombre}</h2>
+            <h2 class="jumbotron bg-dark display-3 pt-5 text-light">${juegostienda[i].nombre}</h2>
             <hr class="text-light">
             </section>`;
             nombre.innerHTML = titulo;
