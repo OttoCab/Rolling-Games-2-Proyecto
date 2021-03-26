f549b72f55d3b72f4b49294a09b783e72f10a0b2
Slaya M. Ostapczuk <slaya.ostapczuk@gmail.com>
2021-03-26 11:27:45 -0300

    termine 404 y agregue los estilos correspondientes

--- js/detalle.js
+++ js/detalle.js
@@ -18,3 +18,26 @@ function leerdatoslocal() {
         }
     }
 }
+
+let detalleJuego = [];
+detalleDelJuego();
+
+function detalleDelJuego() {
+    if (localStorage.length > 0) {
+        //se trae los datos del localStorage y se los pasa a js
+        detalleJuego = JSON.parse(localStorage.getItem('ListaDeJuegos'));
+        //crea la card con la los datos de juego
+        let lista = document.getElementById('JuegoDetalle');
+        //limpar los datos del contenedor 
+        lista.innerHTML = '';
+        for (let i in detalleJuego) {
+            let columna = `
+            <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].desarrolladora}</div>
+            <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].fechaLanzamiento} </div>
+            <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].categoria}</div>
+            <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].clasificacion}</div>
+            `
+            lista.innerHTML += columna;
+        }
+    }
+}
