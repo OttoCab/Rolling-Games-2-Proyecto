790668334e0df2ad675b276213c11fa053fe5799
Slaya M. Ostapczuk <slaya.ostapczuk@gmail.com>
2021-03-28 15:57:59 -0300

    arreglamos el problema de la lectura del localstorage

--- js/detalle.js
+++ js/detalle.js
@@ -6,42 +6,19 @@ function leerdatoslocal() {
         juegostienda = JSON.parse(localStorage.getItem('ListaDeJuegos'));
         console.log('local', juegostienda)
         let nombre = document.getElementById('tituloJuego');
-        let descripcion = document.getElementById('detalle-descrip')
+        //let descripcion = document.getElementById('detalle-descrip')
         nombre.innerHTML = '';
-        descripcion.innerHTML = '';
+        //descripcion.innerHTML = '';
         for (let i in juegostienda) {
             console.log('iii', i)
             let titulo =
-                `<section class="text-center" id="tituloJuego">
-            <h2 class="jumbotron bg-dark display-3 pt-5 text-light">${juegostienda[i].nombre}</h2>
+                `
+            <h2 class="bg-dark display-3 pt-5 text-light">${juegostienda[i].nombre}</h2>
             <hr class="text-light">
-            </section>`;
-            let pDescrip = `<p>${juegostienda[i].descripcion}</p>`;
+            `;
+            //let pDescrip = `<p>${juegostienda[i].descripcion}</p>`;
             nombre.innerHTML = titulo;
-            descripcion.innerHTML = pDescrip;
-        }
-    }
-}
-
-let detalleJuego = [];
-detalleDelJuego();
-
-function detalleDelJuego() {
-    if (localStorage.length > 0) {
-        //se trae los datos del localStorage y se los pasa a js
-        detalleJuego = JSON.parse(localStorage.getItem('ListaDeJuegos'));
-        //crea la card con la los datos de juego
-        let lista = document.getElementById('JuegoDetalle');
-        //limpar los datos del contenedor 
-        lista.innerHTML = '';
-        for (let i in detalleJuego) {
-            let columna = `
-            <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].desarrolladora}</div>
-            <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].fechaLanzamiento} </div>
-            <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].categoria}</div>
-            <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].clasificacion}</div>
-            `
-            lista.innerHTML += columna;
+            //descripcion.innerHTML = pDescrip;
         }
     }
 }
@@ -60,9 +37,35 @@ function armaSlider() {
             slider.innerHTML = slider + `
             <div class="d-flex justify-content-center">
                             <div>
-                                <img src="/img/Games/${imgJuegos[i].imagen}" class="d-block w-100" alt="...">
+                                <img src="/img/Games/${juegostienda[i].imagen}" class="d-block w-100" alt="...">
                             </div>
                             </div>`
+        }
+    }
+}
+
+let detalleJuego = [];
+detalleDelJuego();
+
+
+function detalleDelJuego() {
+    if (localStorage.length > 0) {
+        //se trae los datos del localStorage y se los pasa a js
+        detalleJuego = JSON.parse(localStorage.getItem('ListaDeJuegos'));
+        //crea la card con la los datos de juego
+        let lista = document.getElementById('juegoDetalle');
+        //limpar los datos del contenedor 
+        lista.innerHTML = '';
+        for (let i in juegostienda) {
+            console.log("entro al for");
+            console.log(detalleJuego);
+            let columna = `
+            <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].categoria}</div>
+                <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].descripcion}</div>
+                <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].fechaLanzamiento}</div>
+                <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].clasificacion}</div>
+                <div class="col-lg-4 col-sm-12 my-3">${detalleJuego[i].desarrolladora} </div>`
+            lista.innerHTML += columna;
         }
     }
 }
